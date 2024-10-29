b3e205a538e1fc899207701e533eb34ce3adcd84
Nikita Kuznetsov <50284221+Nikait@users.noreply.github.com>
2024-10-29 23:32:59 +0300

    deleted extra configs

--- conf/config.py
+++ conf/config.py
@@ -5,40 +5,35 @@ from hydra.core.config_store import ConfigStore
 
 @dataclass
 class Train:
-    bsz: int = 64
+    bsz: int = 25
     lr: float = 3e-3
     n_epoch: int = 30
 
-    train_sz: float = 0.8
+    block_size: int = 32
+    max_iters: int = 5000
+    eval_interval: int = 500
+    eval_iters: int = 200
+    n_embd: int = 24
+    n_head: int = 6
+    n_layer: int = 6
+    dropout: float = 0.2
 
-    block_size = 256 # what is the maximum context length for predictions?
-    max_iters = 5000
-    eval_interval = 500
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    eval_iters = 200
-    n_head = 6
-    n_layer = 6
-    dropout = 0.2
-
-    vocab_size = 65
-    block_size = 256
-    n_embed = 24
-    num_heads = 6
-    n_layers = 6
+    vocab_size: int = 65
+    num_heads: int = 6
 
 
 @dataclass
 class Data:
     # path to save dataset
-    data_path: str = "data/IMDB Dataset.csv"
+    data_path: str = "data/input.txt"
     # path to weights
     checkpoints: str = "/weights/"
     # part for train
     train: float = 0.8
     block_size: int = 256
-    
+
     load_checkpoint: bool = False
-    load_checkpoint_path: str = "/weights/chepoint.pt"
+    load_checkpoint_path: str = "/weights/checkpoint.pt"
 
 
 
