180f0a2ef05d89dcee4533eb05e9d34a79f832ee
Nikita Kuznetsov <50284221+Nikait@users.noreply.github.com>
2024-10-29 23:40:28 +0300

    constructed DataLoader from text data (Shakespeare's texts)

--- dataset.py
+++ dataset.py
@@ -1,29 +1,45 @@
-import numpy as np
-
 import torch
-from torch import Tensor
-from torchpack.datasets.dataset import Dataset
+from torch import Tensor, tensor
+
+from typing import Dict, Union
 
 
-
-class Data(Dataset):
+class TextDataset(torch.utils.data.Dataset):
     def __init__(
             self, 
-            x: np.ndarray, 
-            y: np.ndarray, 
-        ) -> None:
+            text: str, 
+            characters: int, 
+            block_size: int, 
+            train: bool=True
+        ):
+        """
+        Construct character level encding-decoding
+        """
+        super().__init__()
+        self.text = text
+        self.characters = characters
+        self.block_size = block_size
+        self.train = train
 
-        # TODO
+        stoi = {s:i for i,s in enumerate(self.characters)}
+        itos = {i:s for i,s in enumerate(self.characters)}
 
-        self.x_data = torch.from_numpy(x)
-        self.y_data = torch.from_numpy(y)
+        self.enc = lambda s: [stoi[c] for c in s]
+        self.dec = lambda l: ''.join([itos[i] for i in l])
+        self.data = tensor(self.enc(self.text), dtype=torch.long)
 
-        self._length = len(self.x_data)
 
+    def __getitem__(self, index: int) -> Dict[str, Union[Tensor, str]]:
+        idx = index # torch.randint(len(self.data) - self.block_size, size=(1,))
+        if self.train:
+            idx = torch.randint(len(self.data) - self.block_size, size=(1,))
+        
+        X = self.data[idx:idx + self.block_size]
+        y = self.data[idx+1 : idx+self.block_size+1]
 
-    def __getitem__(self, ind: int) -> tuple[Tensor, ...]:
-        return self.x_data[ind], self.y_data[ind]
-
+        return (X, y)
 
     def __len__(self) -> int:
-        return self._length
+        if self.train:
+            return 5000
+        return len(self.data) - self.block_size
