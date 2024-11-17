536a91ee526f84ddadb7b69e648dcaea8d1b7c63
Nikita Kuznetsov <50284221+Nikait@users.noreply.github.com>
2024-11-17 22:53:12 +0300

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -9,28 +9,27 @@ class Train:
     lr: float = 3e-3
     n_epoch: int = 30
 
-    block_size: int = 32
+    block_size: int = 4
     max_iters: int = 5000
     eval_interval: int = 500
     eval_iters: int = 200
-    n_embd: int = 24
-    n_head: int = 6
+    n_embd: int = 4
+    n_head: int = 1
     n_layer: int = 6
     dropout: float = 0.2
 
     vocab_size: int = 65
-    num_heads: int = 6
 
 
 @dataclass
 class Data:
     # path to save dataset
-    data_path: str = "data/input.txt"
+    data_path: str = "QSA/h"
     # path to weights
     checkpoints: str = "/weights/"
     # part for train
     train: float = 0.8
-    block_size: int = 256
+    block_size: int = 6
 
     load_checkpoint: bool = False
     load_checkpoint_path: str = "/weights/checkpoint.pt"
